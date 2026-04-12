ed20436b8776a3cd5c62c808e6113e53d9dc374c
Rexihs <fedoreev.4@gmail.com>
2026-04-13 02:34:41 +0300

    Исправленный расчёт всех 4ёх методов

--- function_dp.py
+++ function_dp.py
@@ -5,7 +5,7 @@ def vniigaz_vertical_one(lamda_tube, diameter,
                velocity_liquid, velocity_gas,
                density_liquid, density_gas,
                pressure, sigma,
-               q_liquid, q_gas):
+               q_liquid):
     """
     Функция для расчёта течения ГЖП для вертикальных скважин
     по методике ВНИИГАЗ (модель 1)
@@ -51,7 +51,7 @@ def vniigaz_vertical_two(lamda_tube, diameter,
                      velocity_liquid, velocity_gas,
                      density_liquid, density_gas,
                      pressure, sigma,
-                     q_liquid, q_gas):
+                     q_liquid):
     """
     Функция для расчёта течения ГЖП по методике ВНИИГАЗ (модель 2) 
     для вертикальных скважин.
@@ -70,7 +70,7 @@ def vniigaz_vertical_two(lamda_tube, diameter,
     i0 = lamda_tube/2 * Fr0 + 0.00667 * Bu - 0.0012
 
     Fr = density_gas * velocity_gas**2 / density_liquid / 9.80665 / diameter
-    
+
     if Fr > Fr0:
         i = i0 + lamda_tube/2 * (Fr - Fr0)
     elif Fr < Fr1:
@@ -86,7 +86,7 @@ def vniigaz_inclined(lamda_tube, diameter, alfa,
                     velocity_liquid, velocity_gas,
                     density_liquid, density_gas,
                     pressure, sigma,
-                    q_liquid, q_gas):
+                    q_liquid):
     """
     Функция для расчёта течения ГЖП для наклонных скважин
     по методике ВНИИГАЗ.
@@ -116,7 +116,7 @@ def vniigaz_inclined(lamda_tube, diameter, alfa,
     Fr = density_gas * velocity_gas ** 2 / density_liquid / 9.80665 / diameter
 
     if alfa > 17:
-        # Для углов > 17° используется повернутая коррекция
+        # Для углов > 17°
         if Fr > Fr0_alfa:
             i = i0_alfa + lamda_tube / 2 * (Fr - Fr0_alfa)
         elif Fr < Fr1_alfa:
@@ -133,18 +133,17 @@ def vniigaz_inclined(lamda_tube, diameter, alfa,
             i = Gfr / (Gfr + 1.5)
         else:
             i = i0
-    print(i)
+
     # Финальный градиент давления с учётом наклона
     dp_dz = (i * density_liquid + density_gas * cos_alfa) * 9.80665
     return dp_dz
 
-# --!! Внимание полностью проверить расчёт горизонталного потока !!--
-# --!! Внимание полностью проверить расчёт горизонталного потока !!--
+# FIXME: Отрицательные значения! При расчёте dp через cos(alfa)!
 def vniigaz_horizontal(lamda_tube, diameter, alfa,
                             velocity_liquid, velocity_gas,
                             density_liquid, density_gas,
                             pressure, sigma,
-                            q_liquid, q_gas):
+                            q_liquid):
     """
     Функция для расчёта течения ГЖП по методике ВНИИГАЗ для горизонтальных скважин.
     """
@@ -172,5 +171,7 @@ def vniigaz_horizontal(lamda_tube, diameter, alfa,
         # ileft
         i = cos_alfa_rad * np.exp(-C * Fr**(1/3))
 
-    dp_dz = (i * density_liquid + density_gas * cos_alfa_rad) * 9.80665
+    # FIXME: Отрицательные значения! При расчёте dp через cos(alfa)!
+    # dp_dz = i * density_liquid * 9.80665 + density_gas * 9.80665 * np.cos(alfa)
+    dp_dz = i * density_liquid * 9.80665 + density_gas * 9.80665 * cos_alfa_rad
     return dp_dz
